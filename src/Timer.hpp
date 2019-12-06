e4ce5d1e176a2385987111f431030b123c2a4121
Guilherme Gomes <jguilhermefgomes@gmail.com>
2019-12-06 17:11:34 +0000

    Timer class added

---
+++ src/Timer.hpp
@@ -0,0 +1,25 @@
+/**
+	Timer class
+	Holds game ticks , and timer methods
+**/
+#pragma once
+
+
+
+class Timer
+{
+public:
+	Timer();
+	~Timer();
+
+private:
+	// Timer start game ticks
+};
+
+Timer::Timer()
+{
+}
+
+Timer::~Timer()
+{
+}
